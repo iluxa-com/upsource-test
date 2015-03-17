8968310110ef10e3a3717c5ba7b89ee057e9f9f5
Denis Zhdanov <denis.zhdanov@jetbrains.com>
2015-03-17 17:48:11 +0300

    dummy

--- src/org/denis/Test1.java
+++ src/org/denis/Test1.java
@@ -2,10 +2,6 @@ package org.denis;
 
 import java.util.regex.Pattern;
 
-/**
- * @author Denis Zhdanov
- * @since 16/03/15 16:26
- */
 public class Test1 {
   void test (String s) {
     Pattern pattern = Pattern.compile("\\d+([^\\s]+)");
