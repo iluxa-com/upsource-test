73283920ffbddbbedc74f1f60c7f7f4f016104b0
Denis Zhdanov <denis.zhdanov@jetbrains.com>
2015-03-24 14:15:17 +0300

    xpath inside java injection

--- src/org/denis/Test1.java
+++ src/org/denis/Test1.java
@@ -1,8 +1,23 @@
 package org.denis;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import java.nio.charset.Charset;
 import java.util.regex.Pattern;
 
 public class Test1 {
 
   public static final Pattern PATTERN = Pattern.compile("\\d+([^\\s]+)");
+
+  public void test(XPath xPath) throws XPathExpressionException {
+    if (xPath != null) {
+      xPath.compile("//parent/child::grandchild");
+    }
+  }
+
+  public static void main(String[] args) throws XPathExpressionException {
+    Test1 t = new Test1();
+    System.out.println(Test1.PATTERN);
+    t.test(null);
+  }
 }
