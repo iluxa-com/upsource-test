ef707754c8bb8adab83c12863c9532da4ef25bce
Denis Zhdanov <denis.zhdanov@jetbrains.com>
2015-04-06 11:13:38 +0300

    slf4j library is configured

--- src/org/denis/Test1.java
+++ src/org/denis/Test1.java
@@ -1,5 +1,8 @@
 package org.denis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import java.util.regex.Pattern;
@@ -8,8 +11,11 @@ public class Test1 {
 
   public static final Pattern PATTERN = Pattern.compile("\\d+([^\\s]+)");
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(Test1.class);
+
   public void test(XPath xPath) throws XPathExpressionException {
     if (xPath != null) {
+      LOGGER.info("Testing xpath inside string injection");
       xPath.compile("//parent/child::grandchild");
     }
   }
